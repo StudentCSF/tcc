924311e1bc02da7c51f5a002c6a97f9b5bc47f35
sinyakov.ivan2 <sinyakov.ivan2@wb.ru>
2025-05-09 22:48:10 +0300

    Create Database and fix building project

--- main.c
+++ main.c
@@ -8,21 +8,41 @@
 // //для таймера
 #include <TCC/utils/TCC_timer.h>
 
-int main(void)
-{
-	//test();
-	size_t initLength = 100000000;
-	TCC_MoneyNotes * moneyNotes;
-	TCC_moneyNotesCreate(&moneyNotes, initLength);
-	TCC_moneyNotesLoadTmp(moneyNotes);
-	SDL_Init(SDL_INIT_TIMER);
-	Uint32 startTime = SDL_GetTicks();
-	TCC_Money res = TCC_STSprofitsTax(moneyNotes, 2000);
-	Uint32 endTime = SDL_GetTicks();
-	printf("\n res: %llu\n", res);
-	TCC_moneyNotesDel(&moneyNotes);
-	printf("\n eff: %llu\n", TCC_TIMER_getDiffUS(startTime, endTime));
-	SDL_Quit();
+#include "DataBase/postgres/tcc_db.h"
 
-	return 0;
+int main() {
+    TCC_Error err = TCC_dbInit();
+    if (err != TCC_ERROR_OK) {
+        fprintf(stderr, "Failed to initialize database\n");
+        return 1;
+    }
+
+    TCC_Date date = {.day = 15, .month = 5, .year = 2024};
+    err = TCC_dbInsertMoneyNote(100000, date);
+    if (err != TCC_ERROR_OK) {
+        fprintf(stderr, "Failed to insert test record\n");
+        TCC_dbClose();
+        return 1;
+    }
+
+
+    //test();
+    size_t initLength = 100000000;
+    TCC_MoneyNotes *moneyNotes;
+    TCC_moneyNotesCreate(&moneyNotes, initLength);
+    TCC_moneyNotesLoadTmp(moneyNotes);
+    SDL_Init(SDL_INIT_TIMER);
+    Uint32 startTime = SDL_GetTicks();
+    TCC_Money res = TCC_STSprofitsTax(moneyNotes, 2000);
+    Uint32 endTime = SDL_GetTicks();
+    printf("\n res: %llu\n", res);
+    TCC_moneyNotesDel(&moneyNotes);
+    printf("\n eff: %llu\n", TCC_TIMER_getDiffUS(startTime, endTime));
+    SDL_Quit();
+
+    TCC_dbShowAllRecords();
+
+    TCC_dbClose();
+    return 0;
+
 }
